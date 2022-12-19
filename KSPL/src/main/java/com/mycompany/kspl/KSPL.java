e30cfe0bda5077e11785fd9fbc2835d792b63972
mimi <mimi@mimi-A515-54>
2022-12-18 23:34:51 -0300

    First class created, probably will substituted by a class form.

---
+++ KSPL/src/main/java/com/mycompany/kspl/KSPL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kspl;
+
+/**
+ *
+ * @author mimi
+ */
+public class KSPL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
